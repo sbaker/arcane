40cd7d1abadaa2924b30a77b9d5765878d0aad5a
sbaker <sbaker@logikbug.com>
2016-05-16 22:50:36 -0700

    Added an in-memory test for sanity checks.

--- tests/ArcaneTests/EntityFramework/EntityFrameworkQueryContextTests.cs
+++ tests/ArcaneTests/EntityFramework/EntityFrameworkQueryContextTests.cs
@@ -1,22 +1,16 @@
-﻿using System.Collections.ObjectModel;
-using Arcane;
+﻿using Arcane;
 using Arcane.EntityFramework;
+using ArcaneTests.EntityFramework.Data;
+using ArcaneTests.Models;
 using Microsoft.Data.Entity;
 using Microsoft.Data.Entity.Internal;
-using Xunit;
-using ArcaneTests.EntityFramework.Data;
-using System;
 using System.Linq;
-using ArcaneTests.Models;
+using Xunit;
 
 namespace ArcaneTests.EntityFramework
 {
     public class EntityFrameworkQueryContextTests : ArcaneBaseTest
     {
-        private const int Total = 100;
-        private static readonly DateTime Date = new DateTime(2016, 6, 30, 0, 0, 0, DateTimeKind.Utc);
-
-
         public EntityFrameworkQueryContextTests()
         {
             //ContextFactory<EntityDbContext>.OnContextNeeded = context => new EntityDbContext();
@@ -32,20 +26,7 @@ namespace ArcaneTests.EntityFramework
             {
                 if (db.Database.EnsureCreated())
                 {
-                    for (int i = 1; i <= Total; i++)
-                    {
-                        db.Authors.Add(new Author {
-                            Id = i,
-                            Name = $"First{i} Last{i}",
-                            Books = new Collection<Book> {
-                                new Book {
-                                    Id = i,
-                                    Name = $"C# Development\nBook #{i} of {Total}",
-                                    PublishDate = Date.AddMonths(-(Total - i))
-                                }
-                            }
-                        });
-                    }
+                    var authors = GetAuthors(Total);
 
                     db.SaveChanges();
                 }
@@ -67,7 +48,7 @@ namespace ArcaneTests.EntityFramework
         }
 
         [Fact]
-        public void GetTheFirst24Books()
+        public void GetTheFirst24Authors()
         {
             var entities = Context.Query<Author>().Where(a => a.Id <= 24);
             Assert.True(entities.Count() == 24);
---
+++ tests/ArcaneTests/InMemoryTests/Data/InMemoryQueryContext.cs
@@ -0,0 +1,50 @@
+﻿using Arcane;
+using ArcaneTests.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArcaneTests.InMemoryTests.Data
+{
+    public class InMemoryQueryContext : QueryContext
+    {
+        private List<Author> _authors = new List<Author>();
+
+        public InMemoryQueryContext(Author[] authors)
+        {
+            _authors.AddRange(authors);
+        }
+
+        public override IQuery<T> Query<T>()
+        {
+            var type = typeof(T);
+
+            if (type == typeof(Author))
+            {
+                return new Query<T>(_authors.Cast<T>().AsQueryable());
+            }
+
+            if (type == typeof(Book))
+            {
+                return new Query<T>(_authors.SelectMany(a => a.Books).Cast<T>().AsQueryable());
+            }
+            
+            return new Query<T>(Enumerable.Empty<T>().AsQueryable());
+        }
+
+        public override void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<int> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DisposeCore(bool disposing)
+        {
+        }
+    }
+}
