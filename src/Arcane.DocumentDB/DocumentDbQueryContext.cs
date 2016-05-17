830fddf4475ab5fc20cc96af5313c0a769ec233e
sbaker <sbaker@logikbug.com>
2016-05-17 02:04:48 -0700

    Starting to create the documentdb base functionality.

--- src/Arcane.DocumentDB/DocumentDbQueryContext.cs
+++ src/Arcane.DocumentDB/DocumentDbQueryContext.cs
@@ -1,19 +1,31 @@
-﻿using System;
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Arcane.DocumentDB
 {
     // This project can output the Class library as a NuGet Package.
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-    public class DocumentDbQueryContext : QueryContext
+    public class DocumentDbQueryContext : QueryContext<Database>
     {
+        private DocumentClient _client;
+
         public DocumentDbQueryContext()
         {
+            _client = ContextFactory<DocumentClient>.OnContextNeeded(this);
+        }
+
+        public DocumentDbQueryContext(DocumentClient client)
+        {
+            _client = client;
         }
 
         public override IQuery<T> Query<T>(string name = null)
         {
-            throw new NotImplementedException();
+            return new Query<T>(Enumerable.Empty<T>().AsQueryable());
         }
 
         public override void SaveChanges()
---
+++ tests/ArcaneTests/DocumentDB/DocumentDbQueryContextTests.cs
@@ -0,0 +1,26 @@
+﻿using Arcane;
+using Arcane.DocumentDB;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using System;
+using System.Linq;
+
+namespace ArcaneTests.DocumentDB
+{
+    public class DocumentDbQueryContextTests : ArcaneBaseTest
+    {
+        public DocumentDbQueryContextTests()
+        {
+            ContextFactory<Database>.OnContextNeeded = context => {
+                var client = new DocumentClient(new Uri(""), "");
+
+                var database = client.CreateDatabaseQuery().First();
+
+                return database;
+            };
+
+            Context = new DocumentDbQueryContext();
+        }
+    }
+}
