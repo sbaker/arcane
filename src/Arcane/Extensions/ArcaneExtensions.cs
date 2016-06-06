a698e9ab27f302648771a0f10c3df26205cec917
TheScoobs <sbaker@logikbug.com>
2016-06-06 10:06:48 -0700

    changed the references in project.json to always reference the latest 0.0.* Arcane package.

--- src/Arcane/Extensions/ArcaneExtensions.cs
+++ src/Arcane/Extensions/ArcaneExtensions.cs
@@ -1,5 +1,8 @@
 ﻿namespace Arcane
 {
+    /// <summary>
+    /// A class containing extension methods for use with the Arcane library.
+    /// </summary>
     public static class ArcaneExtensions
     {
     }
