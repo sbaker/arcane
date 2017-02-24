fef1b82a0019b96e42ae5843b76bbe29041d0e95
TheScoobs <sbaker@logikbug.com>
2017-02-24 14:39:16 -0700

    adding raptordb support.

---
+++ src/Arcane.RaptorDB/Data/RaptorDBDataStore.cs
@@ -0,0 +1,46 @@
+﻿using Arcane.Data;
+
+namespace Arcane.RaptorDB.Data
+{
+    public class RaptorDBDataStore : SaveOnDisposeDataStore
+    {
+        /// <summary>
+        /// Inserts or marks the <paramref name="entity"/> as new.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity"></param>
+        public override void Insert<T>(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Updates or marks the <paramref name="entity"/> as modified.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity"></param>
+        public override void Update<T>(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deletes or marks the <paramref name="entity"/> for deletion.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity"></param>
+        public override void Delete<T>(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Calls to the wrapped implementation to persist the changes made.
+        /// </summary>
+        /// <returns></returns>
+        public override int SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
